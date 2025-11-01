86ac64e72374df7e37ee5b8df255a2710cf3dbea
Paul Fleming <paul.stillbreathing@gmail.com>
2025-11-01 13:24:26 +0000

    add failing spec for file_upload_worker

---
+++ spec/lib/file_upload_monitor/file_upload_worker_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe FileUploadMonitor::FileUploadWorker do
+  let(:valid_file_path) { '/tmp/test_file.txt' }
+  let(:invalid_file_path) { '/nonexistent/file.txt' }
+
+  describe '.perform' do
+    context 'with valid file path' do
+      before do
+        allow(File).to receive(:exist?).with(valid_file_path).and_return(true)
+      end
+
+      it 'logs processing message at info level' do
+        expect_any_instance_of(Logger).to receive(:info).with("Processing file: #{valid_file_path}")
+
+        described_class.perform(valid_file_path)
+      end
+    end
+
+    context 'with invalid file path' do
+      before do
+        allow(File).to receive(:exist?).with(invalid_file_path).and_return(false)
+      end
+
+      it 'logs error message' do
+        expect_any_instance_of(Logger).to receive(:error).with("File not found: #{invalid_file_path}")
+
+        described_class.perform(invalid_file_path)
+      end
+    end
+  end
+end
